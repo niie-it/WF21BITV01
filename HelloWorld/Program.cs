b8d1d6a6a123f9b7645eade3e9530e85e1ea88cc
HIENLTH 2023 <hyhien@gmail.com>
2024-02-23 22:43:12 +0700

    Upload source code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _21BITV01
+namespace HelloWorld
 {
 	internal class Program
 	{
--- HelloWorld/Properties/AssemblyInfo.cs
+++ HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("21BITV01")]
+[assembly: AssemblyTitle("HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("21BITV01")]
+[assembly: AssemblyProduct("HelloWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2444c170-cf82-416d-92ef-aae5312355ad")]
+[assembly: Guid("0397eed6-130d-4813-87a5-1475fb69c238")]
 
 // Version information for an assembly consists of the following four values:
 //
