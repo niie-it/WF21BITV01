eaeb88a3675af3912bf3542ac9f4bd33599bd60c
HIENLTH 2023 <hyhien@gmail.com>
2024-02-23 22:38:06 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21BITV01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.InputEncoding = Encoding.UTF8;
+			Console.WriteLine("HELLO NIIE - Chào bạn");
+			Console.Write("Tên bạn là: ");
+			string TenBan = Console.ReadLine();
+			Console.WriteLine($"Chào bạn {TenBan}");
+
+			Console.Write("Year of birth: ");
+			int YOB = int.Parse(Console.ReadLine());
+			int yearOld = DateTime.Now.Year - YOB;
+			Console.WriteLine($"Bạn sinh năm {YOB}, {yearOld} tuổi");
+
+			Console.ReadLine();
+		}
+	}
+}
