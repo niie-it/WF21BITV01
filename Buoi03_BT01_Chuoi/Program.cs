5efe6fc77ecc1416cd2a15e8aa1988955fefed71
HIENLTH 2023 <hyhien@gmail.com>
2024-03-01 07:41:23 +0700

    Tao chuoi ngau nhien

---
+++ Buoi03_BT01_Chuoi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+/*Viết hàm tạo mã ngẫu nhiên có N kí tự
+Input: N - số kí tự
+Output: Chuỗi ngẫu nhiên có N kí tự
+*/
+string TaoChuoiNgauNhien(int N = 5)
+{
+	string pattern = @"qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP!~@#$%^&*[]{}?<>";
+	var sb = new StringBuilder();
+	var rd = new Random();
+	for (int i = 0; i < N; i++)
+	{
+		sb.Append(pattern[rd.Next(0, pattern.Length)]);
+	}
+	return sb.ToString();
+}
+
+//Demo sinh 5 chuỗi ngẫu nhiên
+for(int i = 0; i < 5; i++)
+{
+	Console.WriteLine(TaoChuoiNgauNhien(7));
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 * ST6 123, ST7 456
 
 
-## Nhánh master: Dành cho C# cơ bản
+## Nhánh csbasic: Dành cho C# cơ bản
+
+## Nhánh wfbasic: Windows Form Control
